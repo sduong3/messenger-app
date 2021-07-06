9250dc3bcc02d242bdb8ef8446305faf37960fcb
stvenduong <stvenduong@gmail.com>
2021-07-06 15:43:26 -0700

    Fixed ability to send messages. Also temporarily reversed order of messages to reflect the last message to always end at the bottom.

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,20 +1,25 @@
-import React from "react";
-import { Box } from "@material-ui/core";
-import { SenderBubble, OtherUserBubble } from "../ActiveChat";
-import moment from "moment";
+import React from 'react';
+import { Box } from '@material-ui/core';
+import { SenderBubble, OtherUserBubble } from '../ActiveChat';
+import moment from 'moment';
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
   return (
     <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
+      {messages.reverse().map((message) => {
+        const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
