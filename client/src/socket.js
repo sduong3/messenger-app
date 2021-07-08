262d48c2291dc8f8c73ffa5aa9f9c678a616dad9
stvenduong <stvenduong@gmail.com>
2021-07-08 16:16:43 -0700

    Removed previously commited code that was not relevant for this feature.

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,9 +6,7 @@ import {
   addOnlineUser,
 } from "./store/conversations";
 
-const socket = io(window.location.origin, {
-  withCredentials: true
-});
+const socket = io(window.location.origin);
 
 socket.on("connect", () => {
   console.log("connected to server");
@@ -20,26 +18,9 @@ socket.on("connect", () => {
   socket.on("remove-offline-user", (id) => {
     store.dispatch(removeOfflineUser(id));
   });
-
   socket.on("new-message", (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
   });
-});
-
-/* Error Handling */
-socket.on('connect_error', (err) => {
-  console.error(`connect_error due to ${err.message}`);
-  socket.disconnect();
-});
-
-socket.on("connect_failed", (err) => {
-  console.error(`connect_failed due to ${err.message}`);
-  socket.disconnect();
-});
-
-socket.on("disconnect", (err) => {
-  console.error(`disconnect due to ${err.message}`);
-  socket.disconnect();
 });
 
 export default socket;
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
-const { isUserOnline } = require("../../onlineUsers");
+const onlineUsers = require("../../onlineUsers");
 
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
@@ -28,7 +28,7 @@ router.post("/", async (req, res, next) => {
         user1Id: senderId,
         user2Id: recipientId,
       });
-      if (isUserOnline(sender.id)) {
+      if (onlineUsers.includes(sender.id)) {
         sender.online = true;
       }
     }
